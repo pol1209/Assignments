f126bd3c9ef8ed3e8f39131fab593b8c8ac51570
pol1209 <polamit8@gmail.com>
2025-06-10 12:38:17 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=input('Enter first number:  ')
+b=input('Enter second number: ')
+a=float(a)
+b=float(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition:  ',c)
+print('subtraction:  ',d)
+print('Multiplication:  ',e)
+print('Division:  ',f)
